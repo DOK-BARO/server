c113f5e155dd112ccb315be17eb4cd9c6fa244db
Hyeonjun Park <phjppo0918@gmail.com>
2024-06-16 14:40:58 +0900

    feat : init setting

---
+++ src/test/kotlin/kr/kro/dokbaro/server/ServerApplicationTests.kt
@@ -0,0 +1,15 @@
+package kr.kro.dokbaro.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
