63601838f4f417755f526aa7c36c0c529f8e5586
Hyeonjun Park <phjppo0918@gmail.com>
2024-06-25 13:10:56 +0900

    Feat(DK-6): insert account query

--- src/main/kotlin/kr/kro/dokbaro/server/domain/account/adapter/output/jooq/AccountCommandRepository.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/domain/account/adapter/output/jooq/AccountCommandRepository.kt
@@ -1,19 +1,68 @@
 package kr.kro.dokbaro.server.domain.account.adapter.output.jooq
 
 import kr.kro.dokbaro.server.domain.account.model.Account
+import kr.kro.dokbaro.server.domain.account.model.Role
 import kr.kro.dokbaro.server.domain.account.port.output.ExistAccountPort
 import kr.kro.dokbaro.server.domain.account.port.output.SaveAccountPort
 import org.jooq.DSLContext
+import org.jooq.generated.tables.JAccount
+import org.jooq.generated.tables.JRole
+import org.springframework.stereotype.Repository
 
+@Repository
 class AccountCommandRepository(
 	private val dslContext: DSLContext,
 ) : ExistAccountPort,
 	SaveAccountPort {
-	override fun existBy(socialId: String): Boolean {
-		TODO("Not yet implemented")
+	companion object {
+		private val ACCOUNT = JAccount.ACCOUNT
+		private val ROLE = JRole.ROLE
 	}
 
+	override fun existBy(socialId: String): Boolean =
+		dslContext
+			.fetchExists(
+				dslContext
+					.selectOne()
+					.from(ACCOUNT)
+					.where(ACCOUNT.SOCIAL_ID.eq(socialId)),
+			)
+
 	override fun save(account: Account) {
-		TODO("Not yet implemented")
+		val accountId: Long =
+			insertAccount(account)
+
+		insertRoles(account.roles, accountId)
+	}
+
+	private fun insertAccount(account: Account) =
+		dslContext
+			.insertInto(
+				ACCOUNT,
+				ACCOUNT.SOCIAL_ID,
+				ACCOUNT.PROVIDER,
+				ACCOUNT.CREATED_AT,
+			).values(
+				account.socialId,
+				account.provider.name,
+				account.registeredAt,
+			).returningResult(ACCOUNT.ID)
+			.fetchOneInto(Long::class.java)!!
+
+	private fun insertRoles(
+		roles: Set<Role>,
+		accountId: Long,
+	) {
+		roles.map { it.name }.forEach {
+			dslContext
+				.insertInto(
+					ROLE,
+					ROLE.ACCOUNT_ID,
+					ROLE.NAME,
+				).values(
+					accountId,
+					it,
+				).execute()
+		}
 	}
 }
