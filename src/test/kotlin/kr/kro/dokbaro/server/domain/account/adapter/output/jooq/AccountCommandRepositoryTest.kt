49a4d7c5e1bb174e3f01a4a603e3f93a7eb48408
Hyeonjun Park <phjppo0918@gmail.com>
2024-06-26 00:36:44 +0900

    Test(DK-6): Account Repository test

---
+++ src/test/kotlin/kr/kro/dokbaro/server/domain/account/adapter/output/jooq/AccountCommandRepositoryTest.kt
@@ -0,0 +1,64 @@
+package kr.kro.dokbaro.server.domain.account.adapter.output.jooq
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.extensions.spring.SpringTestExtension
+import io.kotest.extensions.spring.SpringTestLifecycleMode
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kr.kro.dokbaro.server.TestcontainersConfiguration
+import kr.kro.dokbaro.server.domain.account.model.Account
+import kr.kro.dokbaro.server.domain.account.model.Provider
+import org.jooq.Configuration
+import org.jooq.generated.tables.daos.AccountDao
+import org.jooq.generated.tables.daos.RoleDao
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.transaction.annotation.Transactional
+import java.time.Clock
+
+@SpringBootTest
+@Import(TestcontainersConfiguration::class)
+@Transactional
+class AccountCommandRepositoryTest(
+	private val accountCommandRepository: AccountCommandRepository,
+	private val configuration: Configuration,
+) : StringSpec({
+		extensions(SpringTestExtension(SpringTestLifecycleMode.Root))
+
+		val clock = Clock.systemUTC()
+		val accountDao = AccountDao(configuration)
+		val roleDao = RoleDao(configuration)
+
+		"저장을 수행한다" {
+
+			val account =
+				Account.init(
+					"abcdefg",
+					Provider.KAKAO,
+					clock,
+				)
+			val savedAccountId = accountCommandRepository.save(account)
+
+			val accountResult = accountDao.findById(savedAccountId)
+			val roles = roleDao.findAll().filter { it.accountId.equals(savedAccountId) }
+
+			accountResult shouldNotBe null
+			roles.size shouldBe account.roles.size
+		}
+
+		"존재 여부를 판단한다" {
+			val socialId = "abcdefg"
+			accountCommandRepository.save(
+				Account.init(
+					socialId,
+					Provider.KAKAO,
+					clock,
+				),
+			)
+
+			accountCommandRepository.existBy(socialId) shouldBe true
+			accountCommandRepository.notExistBy(socialId) shouldBe false
+			accountCommandRepository.existBy("qwersdaf") shouldBe false
+			accountCommandRepository.notExistBy("qwersdaf") shouldBe true
+		}
+	})
