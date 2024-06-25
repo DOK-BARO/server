2579f251bd3706ce146f16c63a9c014fa7c46d2c
Hyeonjun Park <phjppo0918@gmail.com>
2024-06-26 00:35:37 +0900

    Fix(DK-6): AccountResponse mapping 안되는 현상 수정

--- src/main/kotlin/kr/kro/dokbaro/server/domain/account/port/input/query/dto/AccountResponse.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/domain/account/port/input/query/dto/AccountResponse.kt
@@ -3,7 +3,7 @@ package kr.kro.dokbaro.server.domain.account.port.input.query.dto
 import kr.kro.dokbaro.server.domain.account.model.Account
 
 data class AccountResponse(
-	val id: String,
+	val id: Long,
 	val role: Set<String>,
 	val provider: String,
 ) {
