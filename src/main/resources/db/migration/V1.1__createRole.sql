115e9fa36cd3583e78916367cbad1d3feb39f1a1
Hyeonjun Park <phjppo0918@gmail.com>
2024-06-25 13:12:13 +0900

    Style: reformat

--- src/main/resources/db/migration/V1.1__createRole.sql
+++ src/main/resources/db/migration/V1.1__createRole.sql
@@ -1,7 +1,7 @@
 create table role
 (
 	id         bigint auto_increment primary key,
-	account_id bigint not null references account (id),
+	account_id bigint      not null references account (id),
 	name       varchar(10) null
 );
 
