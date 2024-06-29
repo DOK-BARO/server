6b71f8b24bb45ff940d12a8dabe6f2ed69c06dac
Hyeonjun Park <phjppo0918@gmail.com>
2024-06-29 22:51:26 +0900

    Test(DK-38): 문서 생성 확인용 ControllerTest

---
+++ src/test/kotlin/kr/kro/dokbaro/server/domain/account/adapter/input/command/CancelAccountControllerTest.kt
@@ -0,0 +1,27 @@
+package kr.kro.dokbaro.server.domain.account.adapter.input.command
+
+import com.ninjasquad.springmockk.MockkBean
+import kr.kro.dokbaro.server.abstract.RestDocsTest
+import kr.kro.dokbaro.server.domain.account.port.input.command.DisableAccountUseCase
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(CancelAccountController::class)
+class CancelAccountControllerTest : RestDocsTest() {
+	@MockkBean
+	lateinit var disableAccountUseCase: DisableAccountUseCase
+
+	init {
+		"회원 탈퇴를 수행한다" {
+			val action = performPost("/accounts/cancel")
+
+			action.andExpect(status().isNotImplemented)
+
+			action.andDo(
+				print(
+					"account/cancel-account",
+				),
+			)
+		}
+	}
+}
