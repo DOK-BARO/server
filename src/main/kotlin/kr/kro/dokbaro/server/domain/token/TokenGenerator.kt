a4d938659b6943bce6e41ca5e6ee5d33683116e2
Hyeonjun Park <phjppo0918@gmail.com>
2024-07-12 13:16:43 +0900

    Delete(DK-58): 불필요한 class 삭제

--- src/main/kotlin/kr/kro/dokbaro/server/domain/token/TokenGenerator.kt
+++
@@ -1,5 +0,0 @@
-package kr.kro.dokbaro.server.domain.token
-
-interface TokenGenerator {
-	fun generate(token: TokenClaims): String
-}
--- src/main/kotlin/kr/kro/dokbaro/server/domain/token/refresh/RefreshTokenGenerator.kt
+++
@@ -1,9 +0,0 @@
-package kr.kro.dokbaro.server.domain.token.refresh
-
-import kr.kro.dokbaro.server.domain.token.TokenClaims
-import kr.kro.dokbaro.server.domain.token.TokenGenerator
-import java.util.UUID
-
-class RefreshTokenGenerator : TokenGenerator {
-	override fun generate(token: TokenClaims): String = UUID.randomUUID().toString()
-}
