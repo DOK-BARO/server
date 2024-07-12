51712847f6416d9ac2a5697b36169f6de003ce07
Hyeonjun Park <phjppo0918@gmail.com>
2024-07-12 14:05:58 +0900

    Feat(DK-58): token usecase 기반으로 생성하도록 조정

--- src/main/kotlin/kr/kro/dokbaro/server/domain/auth/model/ProviderAccount.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/domain/auth/model/ProviderAccount.kt
@@ -1,6 +1,7 @@
 package kr.kro.dokbaro.server.domain.auth.model
 
 data class ProviderAccount(
+	val provider: String,
 	val id: String,
 	val name: String?,
 	val email: String?,
--- src/main/kotlin/kr/kro/dokbaro/server/domain/auth/port/input/OAUth2SignUpUseCase.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/domain/auth/port/input/OAUth2SignUpUseCase.kt
@@ -1,7 +1,8 @@
 package kr.kro.dokbaro.server.domain.auth.port.input
 
 import kr.kro.dokbaro.server.domain.auth.port.input.dto.ProviderAuthorizationCommand
+import kr.kro.dokbaro.server.domain.token.model.AuthToken
 
 interface OAUth2SignUpUseCase {
-	fun signUp(command: ProviderAuthorizationCommand)
+	fun signUp(command: ProviderAuthorizationCommand): AuthToken
 }
