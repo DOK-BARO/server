19da0eb2a2c14b45b1c78210cd4cbb8daf9aaee9
Hyeonjun Park <phjppo0918@gmail.com>
2024-07-16 13:36:29 +0900

    Feat(DK-62): token에 유효시간 추가

--- src/test/kotlin/kr/kro/dokbaro/server/domain/auth/adapter/output/kakao/KakaoResourceTokenLoaderTest.kt
+++ src/test/kotlin/kr/kro/dokbaro/server/domain/auth/adapter/output/kakao/KakaoResourceTokenLoaderTest.kt
@@ -1,5 +1,6 @@
 package kr.kro.dokbaro.server.domain.auth.adapter.output.kakao
 
+import com.navercorp.fixturemonkey.kotlin.setExp
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -14,7 +15,13 @@ class KakaoResourceTokenLoaderTest :
 		val kakaoResourceTokenLoader = KakaoResourceTokenLoader(kakaoAuthorizationClient, "token", "aa", "bb", "cc")
 
 		"kakao의 accessToken을 발급받는다" {
-			val sample = FixtureBuilder.give<AuthorizationTokenResponse>().sample()
+			val sample =
+				FixtureBuilder
+					.give<AuthorizationTokenResponse>()
+					.setExp(
+						AuthorizationTokenResponse::accessToken,
+						"asdfasdf",
+					).sample()
 			every { kakaoAuthorizationClient.getAuthorizationToken(any(), any(), any(), any(), any()) } returns sample
 
 			val result: String = kakaoResourceTokenLoader.getToken("token")
