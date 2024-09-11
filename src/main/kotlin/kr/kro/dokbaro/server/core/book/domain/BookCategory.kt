3ac5c3a24f4cb2060efd6e3307c217b38278841a
Hyeonjun Park <phjppo0918@gmail.com>
2024-09-12 00:18:31 +0900

    Feat(DK-221): book category unsaved id 추가

--- src/main/kotlin/kr/kro/dokbaro/server/core/book/domain/BookCategory.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/core/book/domain/BookCategory.kt
@@ -1,11 +1,12 @@
 package kr.kro.dokbaro.server.core.book.domain
 
 data class BookCategory(
-	val id: Long,
+	val id: Long = UNSAVED_BOOK_CATEGORY_ID,
 	val name: String,
 	val details: Collection<BookCategory> = setOf(),
 ) {
 	companion object {
 		const val ROOT_ID = 1L
+		private const val UNSAVED_BOOK_CATEGORY_ID = 0L
 	}
 }
