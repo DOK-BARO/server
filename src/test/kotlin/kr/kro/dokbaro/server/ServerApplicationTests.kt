afaee07dd2f4ebae922b8f6ebc82aa3a60255ad8
Hyeonjun Park <phjppo0918@gmail.com>
2024-09-24 18:30:35 +0900

    Test: 불필요한 의존 제거

--- src/test/kotlin/kr/kro/dokbaro/server/ServerApplicationTests.kt
+++ src/test/kotlin/kr/kro/dokbaro/server/ServerApplicationTests.kt
@@ -1,12 +1,7 @@
 package kr.kro.dokbaro.server
 
-import kr.kro.dokbaro.server.configuration.TestcontainersConfiguration
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 
-@Import(TestcontainersConfiguration::class)
-@SpringBootTest
 class ServerApplicationTests {
 	@Test
 	fun contextLoads() {
