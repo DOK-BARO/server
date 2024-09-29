84911a6fc58fe62163973dd032e5e2619b9054c7
Hyeonjun Park <phjppo0918@gmail.com>
2024-09-29 14:44:39 +0900

    Test(DK-225): book quiz 생성 test

--- src/test/kotlin/kr/kro/dokbaro/server/core/bookquiz/adapter/out/persistence/BookQuizPersistenceAdapterTest.kt
+++ src/test/kotlin/kr/kro/dokbaro/server/core/bookquiz/adapter/out/persistence/BookQuizPersistenceAdapterTest.kt
@@ -3,8 +3,15 @@ package kr.kro.dokbaro.server.core.bookquiz.adapter.out.persistence
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.extensions.spring.SpringTestExtension
 import io.kotest.extensions.spring.SpringTestLifecycleMode
+import io.kotest.matchers.shouldNotBe
 import kr.kro.dokbaro.server.configuration.annotation.PersistenceAdapterTest
+import kr.kro.dokbaro.server.core.book.adapter.out.persistence.repository.jooq.BookRepository
 import kr.kro.dokbaro.server.core.bookquiz.adapter.out.persistence.repository.jooq.BookQuizRepository
+import kr.kro.dokbaro.server.core.member.adapter.out.persistence.entity.jooq.MemberMapper
+import kr.kro.dokbaro.server.core.member.adapter.out.persistence.repository.jooq.MemberRepository
+import kr.kro.dokbaro.server.fixture.domain.bookFixture
+import kr.kro.dokbaro.server.fixture.domain.bookQuizFixture
+import kr.kro.dokbaro.server.fixture.domain.memberFixture
 import org.jooq.DSLContext
 
 @PersistenceAdapterTest
@@ -13,6 +20,15 @@ class BookQuizPersistenceAdapterTest(
 ) : StringSpec({
 		extensions(SpringTestExtension(SpringTestLifecycleMode.Root))
 
+		val memberRepository = MemberRepository(dslContext, MemberMapper())
+		val bookRepository = BookRepository(dslContext)
 		val bookQuizRepository = BookQuizRepository(dslContext)
 		val adapter = BookQuizPersistenceAdapter(bookQuizRepository)
+
+		"신규 book quiz를 생성한다" {
+			val memberId = memberRepository.insert(memberFixture()).id
+			val bookId = bookRepository.insertBook(bookFixture())
+
+			adapter.insert(bookQuizFixture(bookId = bookId, creatorId = memberId)) shouldNotBe null
+		}
 	})
