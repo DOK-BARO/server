4bc7a4a02cc71b773461830fc3b63c9b07f17ee5
Hyeonjun Park <phjppo0918@gmail.com>
2024-10-03 13:33:49 +0900

    Feat(DK-234): 객관식 문제 select 방식 변경

--- src/main/kotlin/kr/kro/dokbaro/server/core/bookquiz/domain/AnswerOptionId.kt
+++
@@ -1,7 +0,0 @@
-package kr.kro.dokbaro.server.core.bookquiz.domain
-
-import kr.kro.dokbaro.server.common.constant.Constants
-
-data class AnswerOptionId(
-	val value: Long = Constants.UNSAVED_ID,
-)
--- src/main/kotlin/kr/kro/dokbaro/server/core/bookquiz/domain/answerstyle/MultipleChoiceAnswer.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/core/bookquiz/domain/answerstyle/MultipleChoiceAnswer.kt
@@ -1,6 +1,5 @@
 package kr.kro.dokbaro.server.core.bookquiz.domain.answerstyle
 
-import kr.kro.dokbaro.server.core.bookquiz.domain.AnswerOptionId
 import kr.kro.dokbaro.server.core.bookquiz.domain.AnswerSheet
 import kr.kro.dokbaro.server.core.bookquiz.domain.Answerable
 import kr.kro.dokbaro.server.core.bookquiz.domain.QuizType
@@ -8,35 +7,34 @@ import kr.kro.dokbaro.server.core.bookquiz.domain.exception.IllegalRegisterSheet
 import kr.kro.dokbaro.server.core.bookquiz.domain.exception.IllegalSubmitSheetFormatException
 
 class MultipleChoiceAnswer private constructor(
-	private val answers: Set<AnswerOptionId>,
+	private val answers: Set<Int>,
 ) : Answerable {
 	companion object {
 		fun from(sheet: AnswerSheet): Answerable {
-			val nums: Collection<Long?> = sheet.answer.map { it.toLongOrNull() }
-
-			if (nums.contains(null)) {
+			if (sheet.answer.any { it.toIntOrNull() == null }) {
 				throw IllegalRegisterSheetFormatException()
 			}
 
-			return MultipleChoiceAnswer(nums.map { AnswerOptionId(it!!) }.toSet())
+			val nums: Set<Int> = sheet.answer.map { it.toInt() }.toSet()
+
+			return MultipleChoiceAnswer(nums)
 		}
 	}
 
 	override fun match(sheet: AnswerSheet): Boolean {
-		val submitAnswers: Collection<Long?> = sheet.answer.map { it.toLongOrNull() }
-
-		if (submitAnswers.contains(null)) {
+		if (sheet.answer.any { it.toIntOrNull() == null }) {
 			throw IllegalSubmitSheetFormatException()
 		}
+		val submitAnswers: Set<Int> = sheet.answer.map { it.toInt() }.toSet()
 
 		if (answers.size != submitAnswers.size) {
 			return false
 		}
 
-		return answers.map { it.value }.containsAll(submitAnswers)
+		return answers.containsAll(submitAnswers)
 	}
 
 	override fun getType(): QuizType = QuizType.MULTIPLE_CHOICE
 
-	override fun getAnswers(): Collection<String> = answers.map { it.value.toString() }
+	override fun getAnswers(): Collection<String> = answers.map { it.toString() }
 }
