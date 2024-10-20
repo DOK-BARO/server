dee5211856c9dc75213f6edaf55bccb3d6e35e6f
Hyeonjun Park <phjppo0918@gmail.com>
2024-10-20 14:39:36 +0900

    Test(DK-252): quizReview fixture 적용

---
+++ src/test/kotlin/kr/kro/dokbaro/server/fixture/domain/QuizReviewFixture.kt
@@ -0,0 +1,20 @@
+package kr.kro.dokbaro.server.fixture.domain
+
+import kr.kro.dokbaro.server.common.constant.Constants
+import kr.kro.dokbaro.server.core.quizreview.domain.QuizReview
+
+fun quizReviewFixture(
+	score: Int = 1,
+	difficultyLevel: Int = 1,
+	comment: String? = null,
+	memberId: Long = Constants.UNSAVED_ID,
+	quizId: Long = Constants.UNSAVED_ID,
+	id: Long = Constants.UNSAVED_ID,
+) = QuizReview(
+	score = score,
+	difficultyLevel = difficultyLevel,
+	comment = comment,
+	memberId = memberId,
+	quizId = quizId,
+	id = id,
+)
