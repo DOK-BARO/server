8e1804f1d13d8c0bcf5d762db76607aa6b5443bb
Hyeonjun Park <phjppo0918@gmail.com>
2024-12-01 00:34:14 +0900

    Refactor: usecase functional interface로 변경

--- src/main/kotlin/kr/kro/dokbaro/server/core/notification/application/port/input/FindAllNotificationUseCase.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/core/notification/application/port/input/FindAllNotificationUseCase.kt
@@ -3,6 +3,6 @@ package kr.kro.dokbaro.server.core.notification.application.port.input
 import kr.kro.dokbaro.server.core.notification.query.NotificationResult
 import java.util.UUID
 
-interface FindAllNotificationUseCase {
+fun interface FindAllNotificationUseCase {
 	fun findAllBy(authId: UUID): Collection<NotificationResult>
 }
--- src/main/kotlin/kr/kro/dokbaro/server/core/notification/application/port/out/UpdateNotificationVisibilityPort.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/core/notification/application/port/out/UpdateNotificationVisibilityPort.kt
@@ -2,6 +2,6 @@ package kr.kro.dokbaro.server.core.notification.application.port.out
 
 import kr.kro.dokbaro.server.core.notification.domain.NotificationVisibility
 
-interface UpdateNotificationVisibilityPort {
+fun interface UpdateNotificationVisibilityPort {
 	fun update(notificationVisibility: NotificationVisibility)
 }
