56963fa560985f4f6a130195b6a33573602bacf0
Hyeonjun Park <phjppo0918@gmail.com>
2024-12-14 15:11:14 +0900

    Fix(DK-357): 이메일 로그인 시 루프 도는 현상 수정

--- src/main/kotlin/kr/kro/dokbaro/server/security/configuration/AuthenticationProviderConfig.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/security/configuration/AuthenticationProviderConfig.kt
@@ -5,14 +5,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.ProviderManager
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 
 @Configuration
 class AuthenticationProviderConfig(
 	private val customProviders: Collection<AuthenticationProvider>,
-	private val authenticationConfiguration: AuthenticationConfiguration,
 ) {
 	@Bean
-	fun AuthenticationManager(): AuthenticationManager =
-		ProviderManager(customProviders.toMutableList(), authenticationConfiguration.authenticationManager)
+	fun AuthenticationManager(): AuthenticationManager = ProviderManager(customProviders.toMutableList())
 }
