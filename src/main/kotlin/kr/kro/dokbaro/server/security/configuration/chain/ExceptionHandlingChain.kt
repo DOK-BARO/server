feb178469990b4a4969510663c5db2775194c059
Hyeonjun Park <phjppo0918@gmail.com>
2025-01-11 16:06:44 +0900

    Fix(DK-443): 인가 에러 발생 시 200 OK 응답하는 현상 수정

--- src/main/kotlin/kr/kro/dokbaro/server/security/configuration/chain/ExceptionHandlingChain.kt
+++ src/main/kotlin/kr/kro/dokbaro/server/security/configuration/chain/ExceptionHandlingChain.kt
@@ -4,16 +4,17 @@ import kr.kro.dokbaro.server.security.annotation.HttpSecurityChain
 import kr.kro.dokbaro.server.security.configuration.SecurityChain
 import kr.kro.dokbaro.server.security.configuration.exception.AuthenticationFailureEntryPoint
 import kr.kro.dokbaro.server.security.configuration.exception.CustomAccessDeniedHandler
+import kr.kro.dokbaro.server.security.jwt.cookie.JwtHttpCookieRemover
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 
 @HttpSecurityChain
 class ExceptionHandlingChain(
-	private val authenticationFailureEntryPoint: AuthenticationFailureEntryPoint,
 	private val accessDeniedHandler: CustomAccessDeniedHandler,
+	private val cookieRemover: JwtHttpCookieRemover,
 ) : SecurityChain {
 	override fun link(origin: HttpSecurity): HttpSecurity =
 		origin.exceptionHandling {
 			it.accessDeniedHandler(accessDeniedHandler)
-			it.authenticationEntryPoint(authenticationFailureEntryPoint)
+			it.authenticationEntryPoint(AuthenticationFailureEntryPoint(cookieRemover))
 		}
 }
